4e37e681ae2cf6e3e1295c258b4b16fa53028c5a
Risk_Hub <riskhub.thinklogical@gmail.com>
2022-11-11 17:39:59 +0530

    Time: 2 ms (73.26%), Space: 48 MB (35.61%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -2,21 +2,12 @@ class Solution {
     public int removeDuplicates(int[] nums) {
         int pos = 1;
         int temp = nums[0];
-        int counter = 1;
-        boolean flag = false;
         for(int i = 1;i < nums.length;i++){
             if(nums[i] != temp){
-                counter++;
                 temp = nums[i];
-                // if(flag){
-                    nums[pos] = nums[i];
-                    pos++;
-                    flag = false;
-                // }
+                nums[pos] = nums[i];
+                pos++;
             }
-            // else {
-            //     flag = true;
-            // }
         }
         return pos;
     }
